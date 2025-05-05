d52e54d091744d71ef8a1481af61ac6fa40aed3e
Aamir <amir.manzoor@immcorporate.com>
2025-05-05 16:37:33 +0500

    list of users

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class userController extends Controller
 {
     //
+    public function index() {
+
+        $users = User::where('id', '!=', Auth::user()->id)->get();
+        return view('dashboard', compact('users'));
+    }
+
+    // public function userChat($userId) {
+    //     return view('user-chat', compact('userId'));
+    // }
 }
